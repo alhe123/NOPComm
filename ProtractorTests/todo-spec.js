42c1fc3bf3d8818a77fe33926264bc817176f049
n00byD <nathan.devoll@gmail.com>
2017-10-27 15:15:40 -0500

    Revert "tests"
    This reverts commit 7b3e978b9aa58ceb02b677cc9ca31ac3a0577b07.

--- ProtractorTests/todo-spec.js
+++
@@ -1,8 +0,0 @@
-describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
-  it('should add a todo', function() {
-    browser.ignoreSynchronization = true;
-    browser.get('http://msf-preprod.io');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
-    });
-});
