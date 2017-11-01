c0f7491b6ff05229a9905c13e5f32674bea6d33d
n00byD <nathan.devoll@gmail.com>
2017-11-01 13:18:53 -0500

    Update todo-spec.js

--- ProtractorTests/todo-spec.js
+++ ProtractorTests/todo-spec.js
@@ -2,7 +2,7 @@ describe('CDBU Modern Software Factory UI Test - nateDevollModernSof-env-preprod
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://msf-preprod.cdbu.io');
-    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('submit')).click();
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User\ue007');
+    //element(by.buttonText('submit')).click();
     });
 });
