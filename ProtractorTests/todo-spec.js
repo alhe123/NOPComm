7d6d01f401631055d0912fe9d3e97dd48cd4cd1f
n00byD <nathan.devoll@gmail.com>
2017-11-01 11:48:51 -0500

    Add files via upload

--- ProtractorTests/todo-spec.js
+++ ProtractorTests/todo-spec.js
@@ -2,7 +2,7 @@ describe('CDBU Modern Software Factory UI Test - nateDevollModernSof-env-preprod
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://msf-preprod.cdbu.io');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
 });
