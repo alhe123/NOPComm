5843c4ad36bb4f59f5b9fbec8ab129f66af726d5
alhe123 <37627657+alhe123@users.noreply.github.com>
2018-06-12 15:44:26 -0500

    Add files via upload

--- ProtractorTests/todo-spec.js
+++ ProtractorTests/todo-spec.js
@@ -1,8 +1,8 @@
-describe('CDBU Modern Software Factory UI Test - nateDevollModernSof-env-preprod Environment', function() {
+describe('CDBU Modern Software Factory UI Test - RaffleAh -env Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://msf-preprod.cdbu.io');
-    element(by.css('[name="name"]')).sendKeys('Selenium Test User\ue007');
-    //element(by.buttonText('submit')).click();
+    browser.get('http://ah-preprod.cdbu.io');
+    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
+    element(by.buttonText('Submit')).click();
     });
 });
